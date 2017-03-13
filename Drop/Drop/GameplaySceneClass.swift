8476cf2ada1750a04597d8849c43fd2060d05ed1
hungqt <hqthieu@gmail.com>
2017-03-13 14:56:16 +0100

    Made a rough menu for changing between menu and game scenes.

---
+++ Drop/Drop/GameplaySceneClass.swift
@@ -0,0 +1,17 @@
+//
+//  GameplaySceneClass.swift
+//  Drop
+//
+//  Created by Hung Quang Thieu on 13/03/2017.
+//  Copyright © 2017 Team15. All rights reserved.
+//
+
+import SpriteKit
+
+class GameplaySceneClass: SKScene{
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+}
--- Drop/Drop/MainMenuScene.swift
+++ Drop/Drop/MainMenuScene.swift
@@ -11,8 +11,20 @@ import SpriteKit
 
 class MainMenuScene: SKScene {
     
-    override func didMove(to view: SKView) {
-        
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self);
+            
+            if atPoint(location).name == "Start" {
+                if let scene = GameplaySceneClass(fileNamed: "GameplayScene") {
+                    // Set the scale mode to scale to fit the window
+                    scene.scaleMode = .aspectFill
+                    
+                    // Present the scene
+                    view!.presentScene(scene, transition: SKTransition.doorsOpenVertical(withDuration: TimeInterval(2))); 
+                }
+            }
+        }
     }
 
 }
